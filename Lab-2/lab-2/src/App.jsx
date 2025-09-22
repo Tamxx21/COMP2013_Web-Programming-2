8e43baee8c4cac1a2cd9fcbbb6fa647a118ccb15
Tristan Miguel Garcia <tristanmiguelgarcia29@gmail.com>
2025-09-22 10:06:46 -0400

    This is the final retouch of lab-2

--- Lab-2/lab-2/src/App.jsx
+++ Lab-2/lab-2/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
             <h1>Resort Lite</h1>
             <hr />
             <div className="main-container">
-                <Card image="src\assets\images\1.jpg" place="Indonesia" venue="Gili Air Hotel" rating=" 4.8 ★" price="$589"/>
-                <Card image="src\assets\images\2.jpg" place="Seychelles" venue="Hilton Resort" rating="4.2 ★" price="$629"/>
-                <Card image="src\assets\images\3.jpg" place="US Virgin Islands" venue="Goa Resort" rating="3.5 ★" price="$485"/>
-                <Card image="src\assets\images\4.jpg" place="Bahamas" venue="Kuredu Resort" rating="4.1 ★" price="$729"/>
-                <Card image="src\assets\images\5.jpg" place="Mauritius" venue="Trou D'eau" rating="4.9 ★" price="$877"/>
-                <Card image="src\assets\images\6.jpg" place="Bermuda" venue="Staniel Cay Hotel" rating="3.2 ★" price="$365"/>
+                <Card image="src\assets\images\1.jpg" place="Indonesia" venue="Gili Air Hotel" rating=" 4.8 " price="$589/night"/>
+                <Card image="src\assets\images\2.jpg" place="Seychelles" venue="Hilton Resort" rating="4.2 " price="$629/night"/>
+                <Card image="src\assets\images\3.jpg" place="US Virgin Islands" venue="Goa Resort" rating="3.5 " price="$485/night"/>
+                <Card image="src\assets\images\4.jpg" place="Bahamas" venue="Kuredu Resort" rating="4.1 " price="$729/night"/>
+                <Card image="src\assets\images\5.jpg" place="Mauritius" venue="Trou D'eau" rating="4.9 " price="$877/night"/>
+                <Card image="src\assets\images\6.jpg" place="Bermuda" venue="Staniel Cay Hotel" rating="3.2 " price="$365/night"/>
             </div>
         </>
     );
--- Lab-2/lab-2/src/Components/Card.jsx
+++ Lab-2/lab-2/src/Components/Card.jsx
@@ -1,10 +1,10 @@
 export default function Card(props) {
     return (
         <div className="Card-Component">
-            <img src={props.image} alt="" width="100px"/>
-            <h5>{props.place}</h5>
-            <p>{props.venue}</p>
-            {props.rating}
+            <img src={props.image} alt=""/>
+            <p><b>{props.place}</b></p>
+            <p className="venue">{props.venue}</p>
+            {props.rating > 4 ? <p className="rGreen"> {props.rating} ★</p> : <p className="rRed"> {props.rating} ★</p>}
             <p>{props.price}</p>
         </div>
     );
